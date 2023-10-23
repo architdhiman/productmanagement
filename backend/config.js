e94207b6185b57d5982e52923a353a8f6bf615c0
Archit Dhiman <arcdhiman007@gmail.com>
2023-10-23 20:05:48 +0530

    final commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  `mongodb+srv://archit:architpassword@cluster0.uqkvxhg.mongodb.net/booksstore?retryWrites=true&w=majority`
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
